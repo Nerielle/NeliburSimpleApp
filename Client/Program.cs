5dfe7358d0e1d97b78084513d9099bc2167c3c49
Nerielle <nelya.iva@gmail.com>
2015-06-24 15:05:56 +0300

    Added Client

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Nelibur;
+using Nelibur.ServiceModel.Clients;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SoapServiceClient("SimpleSoapService");
+            for (; ; )
+            {
+                Console.WriteLine("Enter 2 integer values separated by comma..");
+                string[] values = Console.ReadLine().Split(',');
+                if (values.Length != 2)
+                {
+                    continue;
+                }
+                if (!CheckValues(values))
+                {
+                    continue;
+                }
+                Calculate(client, values);
+            }
+        }
+
+        private static void Calculate(SoapServiceClient client, string[] values)
+        { 
+            var sumValuesRequest = new GetSummRequest(int.Parse(values[0]), int.Parse(values[1]));
+            var response = client.Get<SummResponse>(sumValuesRequest);
+            Console.WriteLine("Sum: {0}", response.Result);
+            Console.ReadLine();
+        }
+
+        private static bool  CheckValues(IEnumerable<string> values)
+        {
+            int result;
+            return values.All(x => int.TryParse(x, out result));
+        }
+    }
+}
