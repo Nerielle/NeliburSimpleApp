48deef15eff1a454468250e2d6be7e16292048b5
Nerielle <nelya.iva@gmail.com>
2015-08-13 16:30:40 +0300

    Tests project added

---
+++ Sources/Tests/XUnitSimpleTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class XUnitSimpleTests
+    {
+        [Fact]
+        public void PositiveTest()
+        {
+            Assert.Equal(7, Add(3,4));
+        }
+
+        [Fact]
+        public void NegativeTest()
+        {
+            Assert.Equal(5, Add(3,4));
+        }
+
+        private int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
